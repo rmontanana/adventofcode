6a2793ed56932c17866ecdb41db3f06fad1749bf
Ricardo Montañana <rmontanana@gmail.com>
2022-12-07 00:29:37 +0100

    Day 6

---
+++ 2022/06/problem62.py
@@ -0,0 +1,23 @@
+"""
+--- Part Two ---
+
+Your device's communication system is correctly detecting packets, but still isn't working. It looks like it also needs to look for messages.
+
+A start-of-message marker is just like a start-of-packet marker, except it consists of 14 distinct characters rather than 4.
+
+Here are the first positions of start-of-message markers for all of the above examples:
+
+mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
+bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
+nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
+How many characters need to be processed before the first start-of-message marker is detected?
+"""
+mlen = 14
+with open("input.txt") as f:
+    data = f.read().strip()
+for i in range(len(data) - mlen):
+    if len(set(data[i : i + mlen])) == mlen:
+        print(i + mlen)
+        break
